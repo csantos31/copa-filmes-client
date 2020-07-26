02cdd77d4abd7f1ab7d8942231023dadac5699cf
Cyntia Santos <cyntia.aparecida@hotmail.com>
2020-07-26 17:07:14 -0300

    alphabetical order

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,18 +27,19 @@ export class HomeComponent implements OnInit {
     this.moviesProvider.getMovies().subscribe(data => {
       if (data != null && data != 'undefined') {
         this.movies = data;
+        this.movies.sort(this.dynamicSort("titulo"));
       }
     });
   }
 
   changeCheck(event) {
     if (event.checked == true) {
-      if(this.challengeMovies.length != 8){
+      if (this.challengeMovies.length != 8) {
         this.challengeMovies.push(this.getMovieById(event.source.value));
-      }else{
+      } else {
         event.source.checked = false;
       }
-    }else{
+    } else {
       this.removeMovie(event.source.value);
     }
     console.log(this.challengeMovies);
@@ -51,8 +52,25 @@ export class HomeComponent implements OnInit {
   removeMovie(id) {
     const index: number = this.challengeMovies.findIndex(x => x.id == id);
     if (index !== -1) {
-        this.challengeMovies.splice(index, 1);
-    }        
-}
+      this.challengeMovies.splice(index, 1);
+    }
+  }
+
+  dynamicSort(property) {
+    var sortOrder = 1;
+
+    if (property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+
+    return function (a, b) {
+      if (sortOrder == -1) {
+        return b[property].localeCompare(a[property]);
+      } else {
+        return a[property].localeCompare(b[property]);
+      }
+    }
+  }
 
 }
